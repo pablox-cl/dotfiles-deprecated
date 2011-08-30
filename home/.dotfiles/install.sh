9cf71c55303958215c37ddfcf67b7a15dcb37438
Pablo Olmos de Aguilera Corradini <pablo@glatelier.org>
2011-08-29 23:22:59 -0300

    Add install file

---
+++ home/.dotfiles/install.sh
@@ -0,0 +1,13 @@
+# Symlinks every file of the dotfiles directory after making sure that
+# a backup is created first
+
+# Get a list of every *file* on dotfiles directory and removes its path
+files=$( find -maxdepth 1 -type f ! -iname "install.sh" | xargs -I {} basename {} ./)
+
+for file in $files; do
+  f="${HOME}/.${file}"                # ~/.bashrc
+  if [ -e $f]; then
+    mv -v "${f}" "${f}".old              # backup old files
+  fi
+  ln -sv "${PWD}/${file}" "${f}"
+done
