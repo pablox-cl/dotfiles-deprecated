4dafe001c4996f5f45d56263aed6966810b2f803
Pablo Olmos de Aguilera Corradini <pablo@glatelier.org>
2011-09-05 23:23:43 -0300

    Uses here document instead like 1 million echoes

--- home/.dotfiles/bashrc.d/aliases/001-git.sh
+++ home/.dotfiles/bashrc.d/aliases/001-git.sh
@@ -20,6 +20,7 @@ alias gpo='git push origin'
 alias gdv='git diff -w "$@" | vim -R -'
 alias gc='git commit -v'
 alias gca='git commit -v -a'
+alias gcam='git commit -a -m'
 alias gci='git commit --interactive'
 alias gb='git branch'
 alias gba='git branch -a'
@@ -47,32 +48,33 @@ alias gd='git diff | vim -R -'
 #esac
 
 function git-help() {
-  echo "Git Custom Aliases Usage"
-  echo
-  echo " gcl = git clone"
-  echo " g = git"
-  echo " get = git"
-  echo " ga = git add"
-  echo " gall = git add ."
-  echo " gst/gs = git status"
-  echo " gss = git status -s"
-  echo " gl = git pull"
-  echo " gup = git fetch && git rebase"
-  echo " gp = git push"
-  echo " gd = git diff | mate"
-  echo " gdv = git diff -w \"$@\" | vim -R -"
-  echo " gc = git commit -v"
-  echo " gca = git commit -v -a"
-  echo " gci = git commit --interactive"
-  echo " gb = git branch"
-  echo " gba = git branch -a"
-  echo " gcount = git shortlog -sn"
-  echo " gcp = git cherry-pick"
-  echo " gco = git checkout"
-  echo " gexport = git git archive --format zip --output"
-  echo " gdel = git branch -D"
-  echo " gpo = git push origin"
-  echo " gmu = git fetch origin -v; git fetch upstream -v; git merge upstream/master"
-  echo " gll = git log --graph --pretty=oneline --abbrev-commit"
-  echo
+cat << EOF
+Git Custom Aliases Usage
+
+    gcl = git clone
+    g = git
+    get = git
+    ga = git add
+    gall = git add .
+    gst/gs = git status
+    gss = git status -s
+    gl = git pull
+    gup = git fetch && git rebase
+    gp = git push
+    gd = git diff | vim -R -
+    gdv = git diff -w \"$@\" | vim -R -
+    gc = git commit -v
+    gca = git commit -v -a
+    gci = git commit --interactive
+    gb = git branch
+    gba = git branch -a
+    gcount = git shortlog -sn
+    gcp = git cherry-pick
+    gco = git checkout
+    gexport = git git archive --format zip --output
+    gdel = git branch -D
+    gpo = git push origin
+    gmu = git fetch origin -v; git fetch upstream -v; git merge upstream/master
+    gll = git log --graph --pretty=oneline --abbrev-commit
+EOF
 }
