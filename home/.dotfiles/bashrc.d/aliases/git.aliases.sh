79a2cca5c603168ca0eabea2e885d068d7cece6f
Pablo Olmos de Aguilera Corradini <pablo@glatelier.org>
2011-10-02 18:19:40 -0300

    Add a git commit message alias

--- home/.dotfiles/bashrc.d/aliases/git.aliases.sh
+++ home/.dotfiles/bashrc.d/aliases/git.aliases.sh
@@ -19,6 +19,7 @@ alias gp='git push'
 alias gpo='git push origin'
 alias gdv='git diff -w "$@" | vim -R -'
 alias gc='git commit -v'
+alias gcm='git commit -v -m'
 alias gca='git commit -v -a'
 alias gcam='git commit -a -m'
 alias gci='git commit --interactive'
@@ -51,31 +52,32 @@ function git-help() {
 cat << EOF
 Git Custom Aliases Usage
 
-    gcl = git clone
-    g = git
-    get = git
-    ga = git add
-    gall = git add .
-    gst/gs = git status
-    gss = git status -s
-    gl = git pull
-    gup = git fetch && git rebase
-    gp = git push
-    gd = git diff | vim -R -
-    gdv = git diff -w \"$@\" | vim -R -
-    gc = git commit -v
-    gca = git commit -v -a
-    gcam = git commit -a -m
-    gci = git commit --interactive
-    gb = git branch
-    gba = git branch -a
-    gcount = git shortlog -sn
-    gcp = git cherry-pick
-    gco = git checkout
+    gcl     = git clone
+    g       = git
+    get     = git
+    ga      = git add
+    gall    = git add .
+    gst/gs  = git status
+    gss     = git status -s
+    gl      = git pull
+    gup     = git fetch && git rebase
+    gp      = git push
+    gd      = git diff | vim -R -
+    gdv     = git diff -w \"$@\" | vim -R -
+    gc      = git commit -v
+    gcm     = git commit -v -m
+    gca     = git commit -v -a
+    gcam    = git commit -a -m
+    gci     = git commit --interactive
+    gb      = git branch
+    gba     = git branch -a
+    gcount  = git shortlog -sn
+    gcp     = git cherry-pick
+    gco     = git checkout
     gexport = git git archive --format zip --output
-    gdel = git branch -D
-    gpo = git push origin
-    gmu = git fetch origin -v; git fetch upstream -v; git merge upstream/master
-    gll = git log --graph --pretty=oneline --abbrev-commit
+    gdel    = git branch -D
+    gpo     = git push origin
+    gmu     = git fetch origin -v; git fetch upstream -v; git merge upstream/master
+    gll     = git log --graph --pretty=oneline --abbrev-commit
 EOF
 }
