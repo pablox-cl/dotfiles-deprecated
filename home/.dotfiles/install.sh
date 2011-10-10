b15eee1182c141eaf76c7eb269d72e04dc12320b
Pablo Olmos de Aguilera Corradini <pablo@glatelier.org>
2011-10-10 19:41:42 -0300

    Clean up the install file a bit

--- home/.dotfiles/install.sh
+++ home/.dotfiles/install.sh
@@ -2,8 +2,9 @@
 # a backup is created first
 
 # Get a list of every *file* on dotfiles directory and removes its path
-files=$( find -maxdepth 1 -type f ! -regex ".*\(\.bash$\|\.sh$\|Rakefile\|~$\)" | xargs -I {} basename {} ./)
+files=$( find -maxdepth 1 -type f | grep -vE "(.bash$|.sh$|Rakefile|~$)" | xargs -I {} basename {} ./)
 
+echo "Backing up your files..."
 for file in $files; do
   f="${HOME}/.${file}"                   # ~/.bashrc
   if [ -e $f ]; then
