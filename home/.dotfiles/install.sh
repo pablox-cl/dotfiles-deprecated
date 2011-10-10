73e6905592615a67ad7a64b056cfcea781e3fc8f
Pablo Olmos de Aguilera Corradini <pablo@glatelier.org>
2011-10-09 23:35:34 -0300

    Remove files that are not supposed to be installed

--- home/.dotfiles/install.sh
+++ home/.dotfiles/install.sh
@@ -2,10 +2,10 @@
 # a backup is created first
 
 # Get a list of every *file* on dotfiles directory and removes its path
-files=$( find -maxdepth 1 -type f ! -iname "install.sh" | xargs -I {} basename {} ./)
+files=$( find -maxdepth 1 -type f ! -regex ".*\(\.bash$\|\.sh$\|Rakefile\|~$\)" | xargs -I {} basename {} ./)
 
 for file in $files; do
-  f="${HOME}/.${file}"                # ~/.bashrc
+  f="${HOME}/.${file}"                   # ~/.bashrc
   if [ -e $f ]; then
     mv -v "${f}" "${f}".old              # backup old files
   fi
