267136576fe05c3424023f0c9844fe47e2f994d1
Pablo Olmos de Aguilera Corradini <pablo@glatelier.org>
2011-10-10 19:34:16 -0300

    Add git-utils support and its corresponding rake tasks
    git-utils (https://github.com/ddollar/git-utils) is added as a
    submodule, and the rake task would update it.
    Also its readme has been added to the git-help command.

--- home/.dotfiles/bashrc.d/aliases/git.aliases.sh
+++ home/.dotfiles/bashrc.d/aliases/git.aliases.sh
@@ -79,5 +79,22 @@ Git Custom Aliases Usage
     gpo     = git push origin
     gmu     = git fetch origin -v; git fetch upstream -v; git merge upstream/master
     gll     = git log --graph --pretty=oneline --abbrev-commit
+
+Git-utils help
+
+    git-addremove         # add/remove all changes from the index, similar to hg addremove
+    git-incoming          # shows incoming commits from the tracked (or specified) branch
+    git-index             # show the current staging area
+    git-last              # git last 3 (show the last 3 commits)
+    git-outgoing          # show outgoing commits to the tracked (or specified) branch
+    git-patch             # format a patch and put it into ~/Documents/Patches/<Project>/<branch>.patch
+    git-pull-request      # manipulate github pull requests
+    git-rank-contributors # rank all project contributors
+    git-rm-conflicts      # clean up all merge conflicts
+    git-switch            # like checkout, but with auto-stash
+    git-wip               # store a current "work in progress" commit
+    git-unwip             # break a work in progress commit bringing the changes back to unstaged
+    git-wtf               # show the current status of your git repository, including feature branches
+
 EOF
 }
