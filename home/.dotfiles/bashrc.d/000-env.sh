b5555c697f0fb088d52aa58b456f5c7e45aa206b
Pablo Olmos de Aguilera Corradini <pablo@glatelier.org>
2012-03-12 00:19:48 -0300

    Extend command history

--- home/.dotfiles/bashrc.d/000-env.sh
+++ home/.dotfiles/bashrc.d/000-env.sh
@@ -10,7 +10,7 @@ export PATH EDITOR GREP_COLORS LESS PROJECT_DIR
 
 # For setting history length see HISTSIZE and HISTFILESIZE in bash(1)
 HISTSIZE=1000
-HISTFILESIZE=2000
+HISTFILESIZE=20000
 
 # Don't put duplicate lines in the history. See bash(1) for more options
 # ... or force ignoredups and ignorespace
