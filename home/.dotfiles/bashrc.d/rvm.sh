9e69eb2dd12a41487e3d03802ee4f5cbbf9d7992
Pablo Olmos de Aguilera Corradini <pablo@glatelier.org>
2012-06-23 17:14:47 -0400

    Add rvm completion

--- home/.dotfiles/bashrc.d/rvm.sh
+++ home/.dotfiles/bashrc.d/rvm.sh
@@ -2,3 +2,6 @@
 
 # Load RVM into a shell session *as a function*
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+
+# RVM completion
+[[ -r $rvm_path/scripts/completion ]] && . $rvm_path/scripts/completion
